d67616ded27dcfa309830fe2607544d0bcd7a5bf
root <thiendong.iuh@gmail.com>
2021-06-09 19:15:49 +0700

    add mqtt demo

--- nodejs/0_demo/1_expressJsHelloWord/app.js
+++ nodejs/0_demo/1_expressJsHelloWord/app.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 2000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  let status = 0;
+  if(isHigh == true){
+    status = 1;
+  }
+  res.send('' + status + '-' + counter);
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+var counter = 0, isHigh = false;
+function IncreaseCounter(){
+  if(isHigh == false){
+    isHigh = true;
+    counter++;
+  }
+  else{
+    isHigh = false;
+  }
+
+  setTimeout(IncreaseCounter, 2000);
+}
+
+IncreaseCounter();
